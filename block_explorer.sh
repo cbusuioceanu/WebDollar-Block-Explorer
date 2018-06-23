0a85d3d3e6f4ee20efde8ec426d5081045aafccd
Constantin Busuioceanu <39584380+cbusuioceanu@users.noreply.github.com>
2018-06-23 22:29:29 +0300

    Update block_explorer.sh
    -> script updated

--- block_explorer.sh
+++ block_explorer.sh
@@ -42,43 +42,50 @@ if [[ "$readblknr" =~ ^[[:digit:]]+$ ]]; then
 	### VARS
 	tmpoutput="/tmp/$readblknr-blockchain-explorer.json"
 	wgetblock=$(wget --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0" --output-document="$tmpoutput" https://webdollar.network:5001/block/$readblknr.json -q)
-	getid=$(cat $tmpoutput | cut -d '"' -f4)
-	getblockid=$(cat $tmpoutput | cut -d '"' -f8)
-	gettimestamp=$(cat $tmpoutput | cut -d '"' -f12)
-	getmineraddr=$(cat $tmpoutput | cut -d '"' -f16)
-	gettrxs1=$(cat $tmpoutput | cut -d ',' -f6-12 | cut -d '[' -f2)
-	gettrxs2=$(cat $tmpoutput | cut -d ',' -f13-19)
-	gettrxs3=$(cat $tmpoutput | cut -d ',' -f20-26)
-	gettrxs4=$(cat $tmpoutput | cut -d ',' -f27-33)
-	gettrxs5=$(cat $tmpoutput | cut -d ',' -f34-40)
-	gettrxs6=$(cat $tmpoutput | cut -d ',' -f41-47)
-	gettrxs7=$(cat $tmpoutput | cut -d ',' -f48-54)
-	gettrxs8=$(cat $tmpoutput | cut -d ',' -f55-61)
-	gettrxs9=$(cat $tmpoutput | cut -d ',' -f62-68)
-	gettrxs10=$(cat $tmpoutput | cut -d ',' -f69-75)
-	gettrxs11=$(cat $tmpoutput | cut -d ',' -f76-82)
-	gettrxs12=$(cat $tmpoutput | cut -d ',' -f83-89)
-	gettrxs13=$(cat $tmpoutput | cut -d ',' -f90-96)
-	gettrxs14=$(cat $tmpoutput | cut -d ',' -f97-103)
-	gettrxs15=$(cat $tmpoutput | cut -d ',' -f104-110)
-	gettrxs16=$(cat $tmpoutput | cut -d ',' -f111-117)
-	gettrxs17=$(cat $tmpoutput | cut -d ',' -f118-124)
-	gettrxs18=$(cat $tmpoutput | cut -d ',' -f125-131)
-	gettrxs19=$(cat $tmpoutput | cut -d ',' -f132-138)
-	gettrxs20=$(cat $tmpoutput | cut -d ',' -f139-145)
+	getid=$(jq . $tmpoutput | grep -w id | awk '{print$2}' | sed s'/[,]//g')
+	getblockid=$(jq . $tmpoutput | grep block_id | awk '{print$2}' | sed s'/[,]//g' | head -1)
+	gettimestamp=$(jq . $tmpoutput | grep timestamp | awk 'NR==1{$1=""; print$0}' | sed s'/[",]//g')
+	gethash=$(jq . $tmpoutput | grep -w hash | awk '{print$2}' | sed s'/[",]//g')
+	getnonce=$(jq . $tmpoutput | grep nonce | awk 'NR==1{print$2}' | sed s'/[",]//g')
+	getprevhash=$(jq . $tmpoutput | grep previous_hash | awk '{print$2}' | sed s'/[",]//g')
+	getmineraddr=$(jq . $tmpoutput | grep miner_address | awk '{print$2}' | sed s'/[",]//g')
+	gettrxs1=$(jq . -c $tmpoutput | cut -d ',' -f19-29)
+	gettrxs2=$(jq . -c $tmpoutput | cut -d ',' -f37-47)
+	gettrxs3=$(jq . -c $tmpoutput | cut -d ',' -f55-65)
+	gettrxs4=$(jq . -c $tmpoutput | cut -d ',' -f73-83)
+	gettrxs5=$(jq . -c $tmpoutput | cut -d ',' -f91-101)
+	gettrxs6=$(jq . -c $tmpoutput | cut -d ',' -f109-119)
+	gettrxs7=$(jq . -c $tmpoutput | cut -d ',' -f127-137)
+	gettrxs8=$(jq . -c $tmpoutput | cut -d ',' -f145-155)
+	gettrxs9=$(jq . -c $tmpoutput | cut -d ',' -f163-173)
+	gettrxs10=$(jq . -c $tmpoutput | cut -d ',' -f181-191)
+	gettrxs11=$(jq . -c $tmpoutput | cut -d ',' -f199-209)
+	gettrxs12=$(jq . -c $tmpoutput | cut -d ',' -f217-227)
+	gettrxs13=$(jq . -c $tmpoutput | cut -d ',' -f235-245)
+	gettrxs14=$(jq . -c $tmpoutput | cut -d ',' -f253-263)
+	gettrxs15=$(jq . -c $tmpoutput | cut -d ',' -f271-281)
+	gettrxs16=$(jq . -c $tmpoutput | cut -d ',' -f289-299)
+	gettrxs17=$(jq . -c $tmpoutput | cut -d ',' -f307-317)
+	gettrxs18=$(jq . -c $tmpoutput | cut -d ',' -f325-335)
+	gettrxs19=$(jq . -c $tmpoutput | cut -d ',' -f343-353)
+	gettrxs20=$(jq . -c $tmpoutput | cut -d ',' -f361-371)
 
 	colorgetid="$GREEN$getid$STAND"
 	colorgetblockid="$GREEN$getblockid$STAND"
 	colorgettimestamp="$GREEN$gettimestamp$STAND"
 	colorgetmineraddr="$GREEN$getmineraddr$STAND"
+	colorgethash="$GREEN$gethash$STAND"
+	colorgetnonce="$GREEN$getnonce$STAND"
+	colorgetprevhash="$GREEN$getprevhash$STAND"
 
-	gettrxs=$(if [[ $(cat $tmpoutput | cut -d '"' -f20) == from ]]; then
-	echo -e "Yes!"\\n
+	gettrxs=$(if [[ $(jq . -c $tmpoutput | grep trxs_number | cut -d ':' -f21 | cut -d '"' -f2) == from ]]; then
+	echo -e "${GREEN}Yes!$STAND"\\n
 	declare -a array=("$gettrxs1" "$gettrxs2" "$gettrxs3" "$gettrxs4" "$gettrxs5" "$gettrxs6" "$gettrxs7" "$gettrxs8" "$gettrxs9" "$gettrxs10" "$gettrxs11" "$gettrxs12" "$gettrxs13" "$gettrxs14" "$gettrxs15" "$gettrxs16" "$gettrxs17" "$gettrxs18" "$gettrxs19" "$gettrxs20")
+
 	for i in "${array[@]}";
         do
 		if [[ -n $i ]]; then
-			echo "From: $YELLOW$(echo $i | cut -d '"' -f6)$STAND Amount: $CYAN$(echo $i | cut -d ':' -f4 | cut -d '}' -f1)$STAND  To: $MAGENTA$(echo $i | cut -d '"' -f14)$STAND Fee: $RED$(echo $i | cut -d ',' -f4 | cut -d ':' -f2)$STAND";
+			echo "From: $YELLOW$(echo $i | cut -d '"' -f22)$STAND Amount: $CYAN$(echo $i | cut -d '"' -f25 | sed s'/[:},]//g')$STAND  To: $MAGENTA$(echo $i | cut -d '"' -f32)$STAND Fee: $RED$(echo $i | cut -d '"' -f43 | sed s'/[:]//g')$STAND";
 		fi
 
 	done
@@ -88,8 +95,8 @@ if [[ "$readblknr" =~ ^[[:digit:]]+$ ]]; then
 	fi)
 
 		echo "$showinfo Requesting data for block number $readblknr..."
-		echo -e "           iD=$colorgetid\\n     Block_id=$colorgetblockid\\n    Timestamp=$colorgettimestamp\\nMiner_address=$colorgetmineraddr"
-		echo "Transactions=$gettrxs"
+		echo -e "           ID=$colorgetid\\n     Block_id=$colorgetblockid\\n    Timestamp=$colorgettimestamp\\n        Nonce=$colorgetnonce\\n         Hash=$colorgethash\\nPrevious_Hash=$colorgetprevhash\\nMiner_address=$colorgetmineraddr"
+		echo " Transactions=$gettrxs"
 
 
 else
